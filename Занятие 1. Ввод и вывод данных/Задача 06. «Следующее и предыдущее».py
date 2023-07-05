bbc49261048a12e1882dbb621fb645e7ba4a04f4
consul-k <138245769+consul-k@users.noreply.github.com>
2023-07-05 12:56:24 +0300

    Create Задача 06. «Следующее и предыдущее».py

---
+++ Занятие 1. Ввод и вывод данных/Задача 06. «Следующее и предыдущее».py
@@ -0,0 +1,12 @@
+'''
+
+Условие
+Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы важны!). 
+
+'''
+
+a = int(input())
+b = a + 1
+c = a - 1
+print('The next number for the number', a, 'is', b)
+print('The previous number for the number', a, 'is', c)
