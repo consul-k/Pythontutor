c5d06ad6b7d2305591ceb41fbbc5d971cad222d9
consul-k <138245769+consul-k@users.noreply.github.com>
2023-07-05 13:07:22 +0300

    Create Задача 05. «Минимум из трех чисел».py

---
+++ Занятие 2. Условия/Задача 05. «Минимум из трех чисел».py
@@ -0,0 +1,18 @@
+'''
+
+Условие
+Даны три целых числа. Выведите значение наименьшего из них. 
+
+'''
+
+a = int(input())
+b = int(input())
+c = int(input())
+if a<b and a<c:
+    print(a)
+elif b<a and b<c:
+    print(b)
+elif c<a and c<b:
+    print(c)
+elif a == b and b<c:
+    print(b)
