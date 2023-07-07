1e812beef8a3c5e0c63f48123c2f794054010e24
consul-k <138245769+consul-k@users.noreply.github.com>
2023-07-07 14:49:32 +0300

    Create Задача 16. «Максимальное число идущих подряд равных элементов».py

---
+++ Занятие 06. Цикл while/Задача 16. «Максимальное число идущих подряд равных элементов».py
@@ -0,0 +1,22 @@
+'''
+
+Условие
+Дана последовательность натуральных чисел, завершающаяся числом 0. 
+Определите, какое наибольшее число подряд идущих элементов этой последовательности равны друг другу. 
+
+'''
+
+n = int(input())
+previous = n
+count_len = 1
+count_len_max = 1
+
+while n != 0:
+    n = int(input())
+    if n == previous:
+        count_len += 1
+    else:
+        if count_len > count_len_max:
+            count_len_max = count_len 
+        count_len = 1
+    previous = n
